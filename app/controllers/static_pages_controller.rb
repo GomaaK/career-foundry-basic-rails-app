76d980c8a4ca84dfbca76167b8a73563458af03a
georgebullock <bullock.george@yahoo.com>
2016-07-08 20:52:01 +0200

    5.2 Added search function to navigation. Replaced the form partial with standard form elements. Styled form with Boostrap classes to add spacing.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,12 @@ class StaticPagesController < ApplicationController
   end
 
   def landing_page
-  	redirect_to static_pages_index_path
-  	# @featured_product = Product.first
-  	# @products = Product.limit(5)
+  	# redirect_to static_pages_index_path
+  	@featured_product = Product.first
+  	@products = Product.limit(5)
+  end
+
+  def price_change
   end
 
 end
