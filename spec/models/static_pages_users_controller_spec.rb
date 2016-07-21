35efbdcc595fe1b1e871ae7370c9a3e8ed727374
georgebullock <bullock.george@yahoo.com>
2016-07-21 13:19:30 +0200

    6.3 Created functional test for Users controller to ensure users data is properly loaded at login time and that non-admin users are properly redirected to the root_path if they try to access other users detail pages.

---
+++ spec/models/static_pages_users_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+  @user_1 = let(:user) { User.create!(email: "george@bullocksbikes.com", password: "123456") }
+  @user_2 = let(:user) { User.create!(email: "support@bullocksbikes.com", password: "123456") }
+
+  describe "GET #show" do
+    before "User is logged in" do
+      sign_in user_1
+      
+      it "loads correct user details" do 
+        get :show, id: user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq user
+      end
+
+      it "tries to access user_2 details" do
+        get :show, id: user_2
+        expect(response).to redirect_to(root_path)
+      end  
+    end  
+
+    before "No user is logged in" do 
+      it "redirects to login" do
+        get :show, id: user.id
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end 
+end
