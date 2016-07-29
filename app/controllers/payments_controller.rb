f8ded7561ab441319c7c0882500b1e16455eeb2a
Omar Mowafi <omar.mowafi@gmail.com>
2016-07-30 01:01:06 +0200

    added fixes

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,31 +1,35 @@
-def create
-	
-	@product = params[:product_id]
-	@user = current_user
+class PaymentsController < ApplicationController
 
-	# Set your secret key: remember to change this to your live secret key in production
-	# See your keys here https://dashboard.stripe.com/account/apikeys
-	Stripe.api_key = "sk_test_c3fEt1Psxt73s1amUx5cIaqjs"
+	def create
+		
+		@product = params[:product_id]
+		@user = current_user
 
-	# Get the credit card details submitted by the form
-	token = params[:stripeToken]
+		# Set your secret key: remember to change this to your live secret key in production
+		# See your keys here https://dashboard.stripe.com/account/apikeys
+		Stripe.api_key = "sk_test_c3fEt1Psxt73s1amUx5cIaqjs"
 
-	# Create the charge on Stripe's servers - this will charge the user's card
-	begin
-	  charge = Stripe::Charge.create(
-	    :amount => @product.price.to_i * @product.stripe_price_conversion,  # amount in cents, again
-	    :currency => "EUR",
-	    :source => token,
-	    :description => @product.description
-	  )
-	 	 
-	  if charge.paid
-  		Order.create()
-		end
+		# Get the credit card details submitted by the form
+		token = params[:stripeToken]
 
-	rescue Stripe::CardError => e
-	  # The card has been declined
-	end	
-		redirect_to static_pages_thank_you_path
+		# Create the charge on Stripe's servers - this will charge the user's card
+		begin
+		  charge = Stripe::Charge.create(
+		    :amount => @product.price.to_i * @product.stripe_price_conversion,  # amount in cents, again
+		    :currency => "EUR",
+		    :source => token,
+		    :description => @product.description
+		  )
+		 	 
+		  if charge.paid
+	  		Order.create()
+			end
+
+		rescue Stripe::CardError => e
+		  # The card has been declined
+		end	
+			redirect_to static_pages_thank_you_path
+
+	end
 
 end
