95124a2338661ba7b29316ab32f060ee18baa441
georgebullock <bullock.george@yahoo.com>
2016-08-04 00:00:21 +0200

    6.12 Implemented redis. Configured to run locally and in production via heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,10 @@
 #Redis initializer
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.production?
+
+	$redis = Redis.new(url: ENV["REDIS_URL"])
+
+else	
+
+	$redis = Redis.new(:host => 'localhost', :port => 6379)
+
+end
